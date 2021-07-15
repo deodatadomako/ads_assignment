c302d3958e079a4078c2957af9ec1044149fbfb3
Deodat <deodatadomako@gmail.com>
2021-07-16 00:27:22 +0100

    first commit

---
+++ 219015317_assignment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+ int main()
+{
+    using namespace std;
+    int a;
+    int b;
+
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    cout<<"square root of a is"<<b;
+    
+    b=sqrt(a);
+    cout<<"square root of a is "<<b;
+}
